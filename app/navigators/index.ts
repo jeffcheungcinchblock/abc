b694616a1e92e7a0da75813b635b33e7bad4fcb6
Jerry Lam <jerry.lam@cinchblock.com>
2022-04-06 16:23:49 +0800

    Remove all files

--- app/navigators/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./app-navigator"
-export * from "./auth-navigator"
-export * from "./auth-loading-navigator"
-export * from "./navigation-utilities"
-// export other navigators from here
--- app/screens/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./welcome-demo/welcome-demo-screen"
-export * from "./welcome/welcome-screen"
-export * from "./demo/demo-screen"
-export * from "./demo/demo-list-screen"
-export * from "./error/error-boundary"
-export * from "./auth/signin-screen"
-export * from "./auth/signup-screen"
-// export other screens here
