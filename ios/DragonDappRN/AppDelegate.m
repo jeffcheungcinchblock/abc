aa6f0826249f63145b116720e0f6e0e299104b6e
Lo Chun John <lolojohn147@gmail.com>
2022-04-12 16:23:32 +0800

    IOS downgrade health but can background?

--- ios/DragonDappRN/AppDelegate.m
+++ ios/DragonDappRN/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RCTAppleHealthKit.h"
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -35,6 +36,7 @@ static void InitializeFlipper(UIApplication *application) {
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"DragonDappRN"
                                             initialProperties:nil];
+  [[RCTAppleHealthKit new] initializeBackgroundObservers:bridge];
 
   if (@available(iOS 13.0, *)) {
       rootView.backgroundColor = [UIColor systemBackgroundColor];
