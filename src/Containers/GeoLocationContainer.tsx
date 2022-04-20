bb273895bf4d91555f961f74a74559baf0808fd8
TImothy Lam <timothylam1228@gmail.com>
2022-04-20 11:18:05 +0800

    update ios

---
+++ src/Containers/GeoLocationContainer.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect ,useRef } from 'react'
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
+} from 'react-native'
+import { useDispatch } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import { Brand } from '@/Components'
+import { useTheme } from '@/Hooks'
+import { useLazyFetchOneQuery } from '@/Services/modules/users'
+import { changeTheme, ThemeState } from '@/Store/Theme'
+import { Image } from 'react-native'
+import { IOSHealthKit } from '../Healthkit/iosHealthKit'
+import { GoogleFitKit } from '../Healthkit/androidHealthKit'
+import Buttons from '@/Theme/components/Buttons'
+import BackgroundGeolocation, {
+  Location,
+  Subscription,
+} from 'react-native-background-geolocation'
+
+
+const GeoLocationContainer = () => {
+  const [dist, setDist] = useState()
+    return (
+    <Text>Distance : {dist}</Text>
+      )
+}
