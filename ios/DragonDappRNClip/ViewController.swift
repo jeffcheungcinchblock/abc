cff12f577d632cbeb2565b7d193b498e96d33956
Jerry Lam <jerry.lam@cinchblock.com>
2022-05-04 14:48:22 +0800

    Add app clip

---
+++ ios/DragonDappRNClip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DragonDappRNClip
+//
+//  Created by Jerry Lam on 4/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
