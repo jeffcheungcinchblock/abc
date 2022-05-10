eaab4c372528fc1431383a6b963371ab13af4d39
Jerry Lam <jerry.lam@cinchblock.com>
2022-05-10 17:21:20 +0800

    Fix splash screen crash issue

--- src/Screens/StartupContainer.tsx
+++ src/Screens/StartupContainer.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
-import { ActivityIndicator, View, Text } from 'react-native'
+import { ActivityIndicator, View, Text, Dimensions } from 'react-native'
 import { useTranslation } from 'react-i18next'
+import Video from 'react-native-video'
 import { useTheme } from '@/Hooks'
 import { Brand } from '@/Components'
 import { setDefaultTheme } from '@/Store/Theme'
 import { navigateAndSimpleReset } from '@/Navigators/utils'
+import { useNavigation } from '@react-navigation/native'
 
-const StartupContainer = () => {
+const StartupContainer = (props) => {
   const { Layout, Gutters, Fonts } = useTheme()
 
   const { t } = useTranslation()
@@ -18,7 +20,8 @@ const StartupContainer = () => {
       }, 2000),
     )
     await setDefaultTheme({ theme: 'default', darkMode: null })
-    navigateAndSimpleReset('Main')
+    console.log("HELLO")
+    props.navigation.replace('Application')
   }
 
   useEffect(() => {
@@ -27,6 +30,23 @@ const StartupContainer = () => {
 
   return (
     <View style={[Layout.fill, Layout.colCenter]}>
+      <Video
+        style={{
+          height: Dimensions.get("window").height,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          alignItems: "stretch",
+          bottom: 0,
+          right: 0
+        }}
+        source={require("../Assets/Videos/sample-5s.mp4")}
+        resizeMode="cover"
+        rate={1.0}
+        muted={true}
+        repeat={true}
+        ignoreSilentSwitch="obey"
+      />
       <Brand />
       <ActivityIndicator size={'large'} style={[Gutters.largeVMargin]} />
       <Text style={Fonts.textCenter}>{t('welcome')}</Text>
