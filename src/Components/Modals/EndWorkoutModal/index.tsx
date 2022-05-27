b586f48c9cbb36ee56c8006ec010bee63134c4c3
Timothy Lam <timothy.lam@cinchblock.com>
2022-05-27 12:14:06 +0800

    fix stop alert

--- src/Components/Modals/EndWorkoutModal/index.tsx
+++ src/Components/Modals/EndWorkoutModal/index.tsx
@@ -1,32 +1,27 @@
-import React, { FC, forwardRef } from "react"
-import { View, ViewStyle, TextStyle, Pressable, Text, Image } from "react-native"
+import React, { FC, forwardRef } from 'react'
+import { View, ViewStyle, TextStyle, Pressable, Text, Image } from 'react-native'
 import { useTranslation } from 'react-i18next'
-import { Spacing } from "@/Theme/Variables"
-import { useTheme } from "@/Hooks"
-import { colors } from "@/Utils/constants"
-import ModalBox from 'react-native-modalbox';
+import { Spacing } from '@/Theme/Variables'
+import { useTheme } from '@/Hooks'
+import { colors } from '@/Utils/constants'
+import ModalBox from 'react-native-modalbox'
 import reward from '@/Assets/Images/Modal/reward.png'
-import TurquoiseButton from "@/Components/Buttons/TurquoiseButton"
+import TurquoiseButton from '@/Components/Buttons/TurquoiseButton'
 
 type EndWorkoutModalProps = {
-  onModalClose: () => void,
-  style?: object,
-  ke: number,
-  onActionBtnPress: () => void,
+  onModalClose: () => void
+  style?: object
+  onActionBtnPress: () => void
 }
 
 const MODAL_TEXT: TextStyle = {
-  color: colors.white, textAlign: "center", fontWeight: "bold"
+  color: colors.white,
+  textAlign: 'center',
+  fontWeight: 'bold',
 }
 
 const EndWorkoutModal = forwardRef((props: EndWorkoutModalProps, ref) => {
-
-  const {
-    onModalClose,
-    style,
-    onActionBtnPress,
-    ke,
-  } = props
+  const { onModalClose, style, onActionBtnPress } = props
   const { t } = useTranslation()
   const { Common, Fonts, Gutters, Layout } = useTheme()
 
@@ -35,36 +30,52 @@ const EndWorkoutModal = forwardRef((props: EndWorkoutModalProps, ref) => {
       ref={ref as any}
       backdropPressToClose={false}
       swipeToClose={false}
-      position="center"
-      entry="bottom"
+      position='center'
+      entry='bottom'
       backdrop={true}
       backButtonClose={false}
       onClosed={onModalClose}
       keyboardTopOffset={40}
       animationDuration={500}
       style={{
-        height: '45%',
-        width: "80%",
+        height: '35%',
+        width: '80%',
         backgroundColor: colors.charcoal,
         borderRadius: 20,
-        ...style
+        ...style,
       }}
     >
-      <View style={[Layout.fullWidth, Layout.colCenter, { flex: 4 }]}>
-        <Image source={reward} />
+      <View style={{ flex: 2, justifyContent: 'center' }}>
+        <Text style={[MODAL_TEXT, { fontSize: 16, fontWeight: 'bold' }]}>{t('modalPrompt.caution')}</Text>
       </View>
-      <Text style={[MODAL_TEXT, { fontSize: 18, paddingHorizontal: 14, flex: 2 }]}>{t("modalPrompt.userEarned")}</Text>
-      <Text style={[MODAL_TEXT, { fontSize: 30, fontStyle: "italic", flex: 3 }]}>{ke}KE</Text>
-      <View style={{
-        alignItems: "center",
-        flex: 3
-      }}>
+      <View style={{ flex: 2, justifyContent: 'center', paddingHorizontal: 14 }}>
+        <Text style={[MODAL_TEXT, { fontSize: 16, fontWeight: '400' }]}>{t('modalPrompt.endWorkout')}</Text>
+      </View>
+      <View
+        style={{
+          justifyContent: 'space-around',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          // height: '20%',
+          flex: 3,
+        }}
+      >
+        <TurquoiseButton
+          onPress={onModalClose}
+          text={t('modalPrompt.continue')}
+          // isTransparentBackground
+          containerStyle={{
+            width: '80%',
+            colors: colors.black,
+          }}
+        />
         <TurquoiseButton
           onPress={onActionBtnPress}
-          text={t("close")}
+          text={t('confirm')}
           isTransparentBackground
           containerStyle={{
-            width: 120,
+            width: '80%',
           }}
         />
       </View>
